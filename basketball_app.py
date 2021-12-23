04c9b33087e9a45ed73a40b32e96a389621bd60b
RAVI SHEKHAR TIWARI <tiwari11.rst@gmail.com>
2021-12-23 19:41:02 +0530

    ui updated

--- basketball_app.py
+++ basketball_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import pandas as pd
 import base64
 import matplotlib.pyplot as plt
@@ -7,14 +8,77 @@ import numpy as np
 
 st.title('Basketball Player Status')
 
+with st.beta_container():
+ #navbar 
+#https://bootsnipp.com/snippets/nNX3a     https://www.mockplus.com/blog/post/bootstrap-navbar-template
+   components.html(
+       """
+       <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark" >
+  <a class="navbar-brand" href="https://www.rstiwari.com">Profile</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href=" https://tiwari11-rst.medium.com/">
+          <i class="fa fa-home"></i>
+          Medium
+          <span class="sr-only">(current)</span>
+          </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=" https://happyman11.github.io/">
+          <i class="fa fa-envelope-o">
+            <span class="badge badge-danger">Git Pages</span>
+          </i>
+         
+        </a>
+      </li>
+      
+        <li class="nav-item">
+        <a class="nav-link" href="https://happyman11.github.io/">
+          <i class="fa fa-globe">
+            <span class="badge badge-success">Badges</span>
+          </i>
+         
+        </a>
+      </li>
+          
+        </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link disabled" href="https://ravishekhartiwari.blogspot.com/">
+          <i class="fa fa-envelope-o">
+            <span class="badge badge-warning">Blogspot</span>
+          </i>
+          
+        </a>
+      </li>
+      
+      
+    </ul>
+  
+    
+  </div>
+</nav>
+       """, height=70,
+    )
+
+
 
 st.header('Webscraping of Basketball player  data')
 
 st.markdown("""
 **Important Links**\n
-**Dataset :** https://www.basketball-reference.com \n
-**Profile :** https://www.rstiwari.com \n
-**Blog :** https://tiwari11-rst.medium.com/ \n
+**Dataset :** https://www.basketball-reference.com 
 """)
 
 
@@ -57,4 +121,18 @@ def filedownload(df):
     return href
 
 st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+
+with st.beta_container():
+    components.html(
+     """
+     <div style="position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: white;
+   text-align: center;">
+  <p>Ravi Shekhar Tiwari</p>
+</div>
+     """,height=140,)
 
